667f2e97f663a23f9b598a75e89adae29f89a2d2
Yogesh Patil <officialyogeshp@gmail.com>
2021-05-25 23:20:47 +0530

    First Commit

---
+++ portfolio_fade.php
@@ -0,0 +1,252 @@
+<?php $active ='Home'; 
+// print_r($_GET);
+//$msg = '';
+//if (isset($_GET['msg']) && !empty($_GET['msg'])) {
+//	$msg = $_GET['msg'];
+//}
+ 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>D'Genious Solutions - </title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta property="og:type" content="website" />
+    <meta content="INDEX,FOLLOW" name="Robots" />
+    <meta content="All, FOLLOW" name="GOOGLEBOTS" />
+    <meta content="All, FOLLOW" name="YAHOOBOTS" />
+    <meta content="All, FOLLOW" name="MSNBOTS" />
+    <meta content="All, FOLLOW" name="BINGBOTS" />   
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="canonical" href="https://www.dgeniussolutions.com/index.html" />
+	<link rel="amphtml" href="http://dgeniussolutions.com/amp/index.html" />
+	<link rel="preconnect" href="https://www.dgeniussolutions.com" />
+    <link rel="preconnect" href="https://www.facebook.com" />
+    <link rel="preconnect" href="https://connect.facebook.net" />
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="assets/images/favicon.png">
+	<link rel="apple-touch-icon" href="assets/images/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+	<!-- fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<!-- styles -->
+	<link href="assets/css/dgs-plugins.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/dgs-style.css" rel="stylesheet" type="text/css">
+	<script>
+	window.onload = () => {
+  var date = new Date();
+  var dt = 
+    date.toLocaleString("default", { month: "long" }) +
+    " " +
+    date.getFullYear().toString();
+  var docTitle = document.title + "  " + dt;
+  document.querySelector("title").textContent = docTitle;
+  //document.querySelector("#currentdate").textContent = dt
+};
+	</script>	
+	
+	<script>(function(w, d) { w.CollectId = "60923e127a73a543539f1ad6"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script>	
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N3C3NGL');</script>
+<!-- End Google Tag Manager -->
+	</head>
+	<body class="preloader cursor-anim-enable">
+	
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3C3NGL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	
+		<!-- preloader-loading start -->
+		<div class="preloader__logoload-box">
+			<img class="preloader__logo" src="assets/images/logo/logo-white.png" alt="logo">
+			<div class="preloader__pulse"></div>
+		</div><!-- preloader-loading end -->
+
+		<!-- pointer start -->
+		<div class="pointer js-pointer" id="js-pointer">
+			<i class="pointer__inner fas fa-long-arrow-alt-right"></i>
+			<i class="pointer__inner fas fa-search"></i>
+			<i class="pointer__inner fas fa-link"></i>
+		</div><!-- pointer end -->
+		
+		<?php include("includes/header.php"); ?>
+		
+		<!-- main start -->
+		<main class="js-animsition-overlay" data-animsition-overlay="true">
+			<!-- pos-rel start -->
+			<section class="pos-rel section-bg-dark-1">
+				<!-- follow-slider-bg start -->
+				<div class="js-fade-slider-bg follow-slider-bg one-page-slider">
+					<!-- swiper-wrapper start -->
+					<div class="swiper-wrapper">
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/speakers/revolt-eeEsmrTVH7Q-unsplash.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/black-bike/josh-nuttall-pIwu5XNvXpk-unsplash.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/phone-case/kerttu-jaatinen-j-5LhTTAzL4-unsplash.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/furniture/goashape-RvyJGaoaWrs-unsplash.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/drone/quadcopter-on-wooden-surface-1601217.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/pexels/woman-in-black-sexy-top-2825034.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/home-decor/tom-crew-NUUjVj4YQLA-unsplash.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/sneakers/revolt-164_6wVEHfI-unsplash.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<div class="swiper-slide">
+							<div class="bg-img-cover height-100perc" style="background-image: url(assets/images/projects/sandman/revolt-mBP_MP6F8mc-unsplash.jpg);">
+								<div class="bg-overlay-black"></div>
+							</div>
+						</div><!-- swiper-slide end -->
+					</div><!-- swiper-wrapper end -->
+				</div><!-- follow-slider-bg end -->
+
+				<!-- js-fade-slider start -->
+				<div class="js-fade-slider">
+					<!-- swiper-wrapper start -->
+					<div class="swiper-wrapper">
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link d-inline-block slider-preloader-anim">Speakers</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link d-inline-block slider-preloader-anim tr-delay-01">Black Bike</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link">Phone Case</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link">Furniture</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link">Drone</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link">Pexels</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link">Home Decor</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link">Sneakers</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+
+						<!-- swiper-slide start -->
+						<h2 class="swiper-slide slide-opacity-anim pos-rel d-flex flex-center-center">
+							<span class="d-block container small">
+								<a href="project_drone.html" class="headline-xxl js-pointer-large js-animsition-link">Sandman</a>
+							</span>
+						</h2><!-- swiper-slide end -->
+					</div><!-- swiper-wrapper end -->
+
+					<!-- swiper-button-prev start -->
+					<div class="swiper-button-prev-box after-preloader-anim">
+						<div class="anim-fade">
+							<div class="swiper-button-prev"></div>
+						</div>
+					</div><!-- swiper-button-prev end -->
+					<!-- swiper-button-next start -->
+					<div class="swiper-button-next-box after-preloader-anim">
+						<div class="anim-fade tr-delay-03">
+							<div class="swiper-button-next"></div>
+						</div>
+					</div><!-- swiper-button-next end -->
+
+					<!-- swiper-pagination start -->
+					<div class="pagination-box after-preloader-anim">
+						<div class="anim-fade tr-delay-06">
+							<div class="swiper-pagination counter-pagination"></div>
+						</div>
+					</div><!-- swiper-pagination end -->
+				</div><!-- js-fade-slider end -->
+			</section><!-- pos-rel end -->
+		</main><!-- main end -->
+
+		<!-- scripts -->
+		<script src="assets/js/plugins.js"></script>
+		<script src="assets/js/main.js"></script>
+	</body>
+</html>
---
+++ who-we-are.php
@@ -0,0 +1,201 @@
+<?php $active ='Home'; 
+// print_r($_GET);
+//$msg = '';
+//if (isset($_GET['msg']) && !empty($_GET['msg'])) {
+//	$msg = $_GET['msg'];
+//}
+ 
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+	<meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>Who We Are</title>
+    <meta name="description" content="" />
+    <meta name="keywords" content="" />
+    <meta property="og:type" content="website" />
+    <meta content="INDEX,FOLLOW" name="Robots" />
+    <meta content="All, FOLLOW" name="GOOGLEBOTS" />
+    <meta content="All, FOLLOW" name="YAHOOBOTS" />
+    <meta content="All, FOLLOW" name="MSNBOTS" />
+    <meta content="All, FOLLOW" name="BINGBOTS" />   
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="canonical" href="https://www.dgeniussolutions.com/index.html" />
+	<link rel="amphtml" href="http://dgeniussolutions.com/amp/index.html" />
+	<link rel="preconnect" href="https://www.dgeniussolutions.com" />
+    <link rel="preconnect" href="https://www.facebook.com" />
+    <link rel="preconnect" href="https://connect.facebook.net" />
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+	<link rel="shortcut icon" href="assets/images/favicon.png">
+	<link rel="apple-touch-icon" href="assets/images/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="assets/images/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="assets/images/apple-touch-icon-114x114.png">
+	<!-- fonts -->
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+	<!-- styles -->
+	<link href="assets/css/dgs-plugins.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/dgs-style.css" rel="stylesheet" type="text/css">
+	<script>
+	window.onload = () => {
+  var date = new Date();
+  var dt = 
+    date.toLocaleString("default", { month: "long" }) +
+    " " +
+    date.getFullYear().toString();
+  var docTitle = document.title + "  " + dt;
+  document.querySelector("title").textContent = docTitle;
+  //document.querySelector("#currentdate").textContent = dt
+};
+	</script>
+
+	<script>(function(w, d) { w.CollectId = "60923e127a73a543539f1ad6"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script>
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-N3C3NGL');</script>
+<!-- End Google Tag Manager -->
+	</head>
+	<body class="preloader cursor-anim-enable">
+	
+	<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N3C3NGL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+	
+		<!-- preloader-loading start -->
+		<div class="preloader__logoload-box">
+			<img class="preloader__logo" src="assets/images/logo/logo-white.png" alt="logo">
+			<div class="preloader__pulse"></div>
+		</div><!-- preloader-loading end -->
+
+		<!-- pointer start -->
+		<div class="pointer js-pointer" id="js-pointer">
+			<i class="pointer__inner fas fa-long-arrow-alt-right"></i>
+			<i class="pointer__inner fas fa-search"></i>
+			<i class="pointer__inner fas fa-link"></i>
+		</div><!-- pointer end -->
+
+		<!-- to top btn start -->
+		<a href="#up" class="scroll-to-btn js-headroom js-midnight-color js-smooth-scroll js-pointer-large">
+			<span class="scroll-to-btn__box">
+				<span class="scroll-to-btn__arrow"></span>
+			</span>
+		</a><!-- to top btn end -->
+
+		<!-- scroll down btn start -->
+		<a href="#down" class="scroll-to-btn to-down js-headroom js-midnight-color js-smooth-scroll js-pointer-large js-scroll-btn">
+			<span class="scroll-to-btn__box">
+				<span class="scroll-to-btn__arrow"></span>
+			</span>
+		</a><!-- scroll down btn end -->
+
+		<?php include("includes/header.php"); ?>
+		
+		<!-- main start -->
+		<main class="js-animsition-overlay" data-animsition-overlay="true">
+			<!-- page head start -->
+			<section id="up" class="pos-rel section-bg-dark-1">
+				<!-- pos-rel start -->
+				<div class="pos-rel flex-min-height-100vh">
+					<div class="container padding-top-bottom-120 after-preloader-anim">
+						<h2 class="headline-xxxs hidden-box">
+							<span class="anim-slide">Who We Are</span>
+						</h2>
+						<h2 class="subhead-xxl margin-top-20 anim-text-reveal tr-delay-03">A team of passionate marketers who bring tailored & tested solutions to you.<br> 
+                                                We plan marketing differently! 
+                                                </h2>
+					</div>
+				</div><!-- pos-rel end -->
+			</section><!-- page head end -->
+                        <section class="pos-rel section-bg-light-2" data-midnight="black">
+				<!-- mision vision -->
+				    <div class="pos-rel">
+					<!-- flex-container start -->
+					<div class="container flex-container padding-top-60 padding-bottom-120">
+						<!-- column start -->
+						<div class="six-columns column-100-100 padding-top-60">
+							<div class="column-r-margin-40-999 js-scrollanim js-scrollanim-active text-center padding-left-right-50">
+								<h2 class="anim-text-fill text text-center">
+								<span class="text-center dgshdtxt">Our Vision</span></h2>
+                                                            <p class="body-text-s text-color-black">Create a business world full of prosperity, meaning and acceptance to all. THE HIGHLIGHT BEING ABLE TO CREATE WORK VALUE THROUGH EMOTIONAL CURRENCY.</p>					
+							</div>
+						</div><!-- column end -->
+						<!-- column start -->
+						<div class="six-columns column-100-100 padding-top-60">
+							<div class="column-r-margin-40-999 js-scrollanim js-scrollanim-active text-center padding-left-right-50">
+								<h2 class="anim-text-fill text text-center">
+								<span class="text-center dgshdtxt">Our Mission</span></h2>
+                                                            <p class="body-text-s text-color-black">Our mission is to be experts in the digital front of marketing and promotion that delivers measurable results to its clients on a daily basis.</p>					
+							</div>
+						</div><!-- column end -->
+					</div><!-- flex-container end -->
+				</div><!-- pos-rel end -->
+			</section><!-- mision vision end-->
+			<!-- work process start -->
+			<section id="down" class="pos-rel section-bg-light-1" data-midnight="black">
+				<!-- pos-rel start -->
+				<div class="pos-rel flex-min-height-100vh">
+					<!-- container start -->
+					<div class="container padding-top-bottom-120">
+						<h2 class="headline-xxs text-color-black hidden-box">
+							<span class="anim-slide js-scrollanim">Our Approach</span>
+						</h2>
+                                             <p class="body-text-s text-color-black margin-top-20">Team D'Genius has its share of masterminds who play the game collaboratively to achieve our client's goals. Thereby stating, "our Approach is based on testing, innovating and making breakthroughs, leading our clients to the best results."</p>
+						<!-- flex-container start -->
+						<div class="flex-container padding-top-30">
+							<!-- column start -->
+							<div class="four-columns column-100-100 padding-top-20">
+								<div class="column-r-margin-40-999 js-scrollanim">
+									<span class="subhead-xxl text-color-red d-block hidden-box">
+										<span class="anim-slide">01</span>
+									</span>
+									<h3 class="headline-xxxs text-color-black margin-top-30 hidden-box">
+										<span class="anim-slide tr-delay-01">With Agility</span>
+									</h3>
+									<p class="body-text-s text-color-black margin-top-20 anim-text-reveal tr-delay-02">Like great athletes, we don't just run, we sprint. We provoke ourselves to set goals constantly requiring us to strive. Clients appreciate how we stretch past limits to get you the results needed. It's the client appreciation we chase. The goal is to cross the finish line first and hope we don't pull a muscle along the way.</p>
+								</div>
+							</div><!-- column end -->
+
+							<!-- column start -->
+							<div class="four-columns column-100-100 padding-top-20">
+								<div class="column-r-margin-40-999 js-scrollanim">
+									<span class="subhead-xxl text-color-red d-block hidden-box">
+										<span class="anim-slide">02</span>
+									</span>
+									<h3 class="headline-xxxs text-color-black margin-top-30 hidden-box">
+										<span class="anim-slide tr-delay-01">With Creativity</span>
+									</h3>
+									<p class="body-text-s text-color-black margin-top-20 anim-text-reveal tr-delay-02">To get the best ideas, you've got to dig deep. Its how we uncover unique ways to tell your story. With an industry that evolves on a minute-by-minute basis, we need to keep up to stay ahead. We need to try new formats, explore new channels and constantly strive to be the best.</p>
+                                                         </div>
+							</div><!-- column end -->
+
+							<!-- column start -->
+							<div class="four-columns column-100-100 padding-top-20">
+								<div class="column-r-margin-40-999 js-scrollanim">
+									<span class="subhead-xxl text-color-red d-block hidden-box">
+										<span class="anim-slide">03</span>
+									</span>
+									<h3 class="headline-xxxs text-color-black margin-top-30 hidden-box">
+										<span class="anim-slide tr-delay-01">With Transparency</span>
+									</h3>
+									<p class="body-text-s text-color-black margin-top-20 anim-text-reveal tr-delay-02">We're honest- and don't keep our clients in the dark. We believe in open communication and working together. There's a reason our office has glass walls. What you see is what you get, and we guarantee you'll like it. The blueprint. The game plan. The ROI. Whatever you can call it, we pursue goals with purpose.</p>
+								</div>
+							</div><!-- column end -->
+						</div><!-- flex-container end -->
+					</div><!-- container end -->
+				</div><!-- pos-rel end -->
+			</section><!-- work process end -->
+		</main><!-- main end -->
+
+		<?php include("includes/footer.php"); ?>
+
+		<!-- scripts -->
+		<script src="assets/js/plugins.js"></script>
+		<script src="assets/js/footer-reveal.js"></script>
+		<script src="assets/js/main.js"></script>
+		<script src="assets/js/footer-reveal_init.js"></script>
+	</body>
+</html>
